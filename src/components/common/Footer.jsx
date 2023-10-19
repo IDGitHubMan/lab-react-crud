3ec6d5ef27be87e0bc29f78aa77bc7dd936586e9
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-10-19 09:51:38 -0400

    Update the footer.

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <p>ScreenViews. An application created by YOUR_NAME.</p>
+      <p>ScreenViews. An application created by Isaiah Desrosiers.</p>
     </footer>
   );
 }
